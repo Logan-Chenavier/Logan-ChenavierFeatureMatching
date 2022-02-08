0eac9e39c77e3d06d763fcb250b185da5d00c9c0
Logan Chenavier <loganchenavier@Logans-MacBook-Pro.local>
2022-02-08 11:34:37 +0100

    webapi

--- Logan.Chenavier.FeatureMatching.WebApi/Program.cs
+++ Logan.Chenavier.FeatureMatching.WebApi/Program.cs
@@ -1,7 +1,9 @@
+using Logan.Chenavier.FeatureMatching;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<ObjectDetection>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
